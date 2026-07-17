637f662a665583f320bda2be2680b05af1007f05
aryanaggarwal121207-a11y <aryanaggarwal121207@gmail.com>
2026-07-17 11:06:23 +0530

    Update admin.tsx

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,3 +1,4 @@
+import { OrderDetailsDialog } from "@/components/admin/OrderDetailsDialog";
 import { DashboardCards } from "@/components/admin/DashboardCards";
 import { useEffect, useState } from "react";
 import { createFileRoute, Link } from "@tanstack/react-router";
